3833883ea18cce2d48548cfb59401d439994fe07
Mozgus <retailnet@rambler.ru>
2018-09-02 16:46:36 +0300

    #4 Tabs for show incomes, expenses and balance

---
+++ app/src/main/java/com/berryjam/moneytracker/ItemsFragment.java
@@ -0,0 +1,61 @@
+package com.berryjam.moneytracker;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.berryjam.moneytracker.dummy.DummyData;
+
+public class ItemsFragment extends Fragment {
+    public static final String KEY_TYPE = "type";
+    public static final int TYPE_INCOMES = 1;
+    public static final int TYPE_EXPENSES = 2;
+
+    public static ItemsFragment newInstance(int type) {
+        ItemsFragment fragment = new ItemsFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ItemsFragment.KEY_TYPE, type);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    public ItemsFragment() {
+        // Required empty public constructor
+    }
+
+    private ItemsAdapter adapter;
+    private int type;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        if (args != null) {
+            type = args.getInt(KEY_TYPE);
+        }
+        adapter = new ItemsAdapter();
+        adapter.setItems(DummyData.getDummyItems());
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_items, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+    }
+
+}
--- app/src/main/java/com/berryjam/moneytracker/MainActivity.java
+++ app/src/main/java/com/berryjam/moneytracker/MainActivity.java
@@ -1,14 +1,25 @@
 package com.berryjam.moneytracker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    MainPagesAdapter mainPagesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        mainPagesAdapter = new MainPagesAdapter(getSupportFragmentManager(), this);
+        viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(mainPagesAdapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
 }
